e4fc7fea753b5a14f2768b60b952446c852c1ba4
André Pinto <andredasilvapinto@gmail.com>
2014-10-26 01:57:39 +0100

    Fixing indentation

--- dtdns.go
+++ dtdns.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"net/http"
-	"os"
-    "fmt"
-    "io/ioutil"
+"net/http"
+"os"
+"fmt"
+"io/ioutil"
 )
 
 const filename string = "ip.dat"
@@ -12,9 +12,9 @@ const hostname string = "HOSTNAME"
 const password string = "PASSWORD"
 
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
 
 func getAndRead(url string) string {
@@ -30,19 +30,19 @@ func getAndRead(url string) string {
 
 func update(ip string) {
 	body := getAndRead("https://www.dtdns.com/api/autodns.cfm?id=" + hostname + "&pw=" + password)
-    
-    if body == ("Host " + hostname + " now points to " + ip + ".\n") {
-	    err := ioutil.WriteFile(filename, []byte(ip), 0644)
-	    check(err)
-    }
+	
+	if body == ("Host " + hostname + " now points to " + ip + ".\n") {
+		err := ioutil.WriteFile(filename, []byte(ip), 0644)
+		check(err)
+	}
 
-    fmt.Println(body)
+	fmt.Println(body)
 }
 
 func main() {
 	ip := getAndRead("http://echoip.com")
 
-    if _, err := os.Stat(filename); os.IsNotExist(err) {
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
 		update(ip)
 	} else {
 		storedIp, err := ioutil.ReadFile(filename)
